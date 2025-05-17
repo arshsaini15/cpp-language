47ead69cdc515eb468a259e3e33c6f1cf1161d46
arshsaini15 <arshdeepp015@gmail.com>
2025-05-17 21:13:21 +0530

    Added Folders

--- Sliding Window/sw3.cpp
+++ Sliding Window/sw3.cpp
@@ -1,15 +1,31 @@
-// Longest Substring With K Unique Character.
+// Longest Substring With K Unique Character (Variable Sized).
 
 #include<iostream>
 #include<unordered_map>
 using namespace std;
 
 int LongestSubstring(string s, int k) {
-    if(k == :)
+    int i=0, j=0, maxLen=0;
+    unordered_map<char, int> mp;
+    while(j < s.length()) {
+        char c = s[j];
+        mp[c]++;
+        if(mp.size() > k) {
+            while(mp.size() > k) {
+                mp[s[i]]--;
+                if(mp[s[i]] == 0) mp.erase(s[i]);
+                i++;
+            }
+        } else if(mp.size() == k) {
+            maxLen = max(maxLen, j-i+1);
+        }
+        j++;
+    }
+    return maxLen;
 }
 
 int main() {
-    int k = 5;
+    int k = 3;
     string s = "aabacebebebe";
     cout << LongestSubstring(s, k);
 }
